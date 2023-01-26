ac51103117eb16d552cbbc8b373eb48324858343
liinahamari <vasiliev.roman@pm.me>
2023-01-26 19:01:34 -0300

    Add slider and mechanism detecting successful initialization of library in Sample.

--- sample/src/main/java/dev/liinahamari/lowbatterynotifier/MainActivity.kt
+++ sample/src/main/java/dev/liinahamari/lowbatterynotifier/MainActivity.kt
@@ -4,19 +4,39 @@ import android.os.Bundle
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
 import android.os.StrictMode.VmPolicy
-import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
 import dev.liinahamari.low_battery_notifier.LowBatteryNotifier
+import dev.liinahamari.lowbatterynotifier.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val ui by viewBinding(ActivityMainBinding::bind)
     private var lowBatteryNotifier: LowBatteryNotifier? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableStrictMode()
         super.onCreate(savedInstanceState)
-        findViewById<Button>(R.id.button).setOnClickListener {
-            lowBatteryNotifier = LowBatteryNotifier.also {
-                it.init(application, requestStroboscopeFeature = true, lowBatteryThresholdLevel = 100)
+        ui.initLibBtn.setOnClickListener { initLowBatteryNotifierLib() }
+    }
+
+    override fun onDestroy() {
+        lowBatteryNotifier = null
+        super.onDestroy()
+    }
+
+    private fun initLowBatteryNotifierLib() {
+        lowBatteryNotifier = LowBatteryNotifier.also {
+            try {
+                it.init(
+                    application,
+                    requestStroboscopeFeature = true,
+                    lowBatteryThresholdLevel = ui.thresholdSlider.value.toInt()
+                )
+                ui.initLibBtn.isEnabled = false
+            } catch (e: Throwable) {
+                ui.initLibBtn.isEnabled = true
+                Toast.makeText(this, e.stackTraceToString(), Toast.LENGTH_LONG).show()
             }
         }
     }
